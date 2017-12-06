24d1c44c1fe6a2b0700ec3d74327490d546d3b34
祥平 佐藤 <150115@jc-21.jp>
2017-12-06 12:56:24 +0900

    変更２

--- SQLite2121/SQLite2121/App.xaml.cs
+++ SQLite2121/SQLite2121/App.xaml.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace SQLite2121
 {
     public partial class App : Application
     {
-        public App()
+        //データベースのパスを格納
+        public static string dbPath;
+
+
+        public App(string dbPath)
         {
+            //AppのdbPathに引数のパスを設定します
+            App.dbPath = dbPath;
+
+
             InitializeComponent();
 
-            MainPage = new SQLite2121.MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
